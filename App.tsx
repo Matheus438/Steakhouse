6edc1787a8e9c8852c9396f0bacb1065d0057e3e
Matheus438 <matheusbispoa3@gmail.com>
2024-04-02 15:36:16 -0300

    commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import FlatListCardapio from "./src/FlatListCardapio";
 import Carrinho from "./src/Carrinho";
+import CadastroProduto from "./src/screens/Cadastro";
 
 function App(): React.JSX.Element {
   return (
-    <FlatListCardapio />
+    /*<FlatListCardapio />*/
     /*<Carrinho />*/
+    <CadastroProduto/>
   );
 }
 
---
+++ src/interface/ProdutoInterface.ts
@@ -0,0 +1,7 @@
+interface Produto {
+    id: number;
+    nome: string;
+    ingredientes: string;
+    preoco: number;
+    image: any;
+}
