e6440e2a4d2c8264e2b46ba3397a6ec479dfae55
Matheus438 <matheusbispoa3@gmail.com>
2024-04-04 15:51:55 -0300

    pagina cadastro

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import FlatListCardapio from "./src/FlatListCardapio";
 import Carrinho from "./src/Carrinho";
 import CadastroProduto from "./src/screens/Cadastro";
+import CadastroSteakhouse from "./src/screens/CadastroSteakhouse";
 
 function App(): React.JSX.Element {
   return (
     /*<FlatListCardapio />*/
-    /*<Carrinho />*/
-    <CadastroProduto/>
+      /*<Carrinho/>*/
+    /*<CadastroProduto/>*/
+    <CadastroSteakhouse/>
   );
 }
 
